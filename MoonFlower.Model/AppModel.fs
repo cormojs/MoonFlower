27edcfc2e8fe05c56de7f83b2e355b2d37bc4fbe
cormojs <cormorant2+github@gmail.com>
2017-05-06 05:21:48 +0900

    Modelでの認証処理を追加

--- MoonFlower.Model/AppModel.fs
+++ MoonFlower.Model/AppModel.fs
@@ -12,16 +12,35 @@ type AppModel() =
     do
         ServicePointManager.SecurityProtocol <- SecurityProtocolType.Tls12
 
-    member val Registrations: Map<string, AppRegistration>= Map.empty with get, set
+    member val Registrations: Map<string, AppRegistration> = Map.empty with get, set
+    member val Accounts: Map<string, YourAccount> = Map.empty with get, set
+
     member this.RegisterApp(host: string): Async<AuthenticationClient> =
         match this.Registrations |> Map.tryFind host with
         | Some regist -> async { return AuthenticationClient(regist) }
         | None -> async {
-            let url = sprintf "https://%s" host
-            Debug.WriteLine url
             let client = AuthenticationClient(host)
             let! regist = client.CreateApp(appName, Scope.Read + Scope.Write + Scope.Follow)
                           |> Async.AwaitTask
             this.Registrations <- this.Registrations.Add(host, regist)
             return client
+        }
+
+    member this.ConnectAccount(host: string, code: string) =
+        match this.Registrations |> Map.tryFind host with
+        | None -> async { return None }
+        | Some regist -> async {
+            let client = AuthenticationClient(regist)
+            let! auth = client.ConnectWithCode(code) |> Async.AwaitTask
+            let! account = this.AddAccount(regist, auth)
+            return Some (auth, account)
+        }
+
+    member this.AddAccount(regist: AppRegistration, auth: Auth) =
+        async {
+            let client = MastodonClient(regist, auth)
+            let! account = client.GetCurrentUser() |> Async.AwaitTask
+            let yourAccount = { Auth = auth; App = regist; Detail = account; }
+            this.Accounts <- this.Accounts.Add(auth.AccessToken, yourAccount)
+            return yourAccount
         }
---
+++ MoonFlower.Model/YourAccount.fs
@@ -0,0 +1,12 @@
+﻿namespace MoonFlower.Model
+
+open Mastonet
+open Mastonet.Entities
+
+type YourAccount =
+    {
+        Auth: Auth; 
+        App: AppRegistration;
+        Detail: Account;
+    }
+    member this.Client = MastodonClient(this.App, this.Auth)
