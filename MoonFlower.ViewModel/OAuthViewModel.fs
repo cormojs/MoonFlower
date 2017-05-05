6a94da1c8a48dc5fb4099e5528b4d032de164ca4
cormojs <cormorant2+github@gmail.com>
2017-05-06 05:22:11 +0900

    アプリケーション認証処理を追加

--- MoonFlower.ViewModel/OAuthViewModel.fs
+++ MoonFlower.ViewModel/OAuthViewModel.fs
@@ -4,14 +4,15 @@ open System.Diagnostics
 
 open ViewModule
 open ViewModule.FSharp
+open ViewModule.Validation.FSharp
 
 open MoonFlower.Model
 
 type OAuthViewModel(app: AppModel) as self =
     inherit ViewModelBase()
 
-    let hostName = self.Factory.Backing(<@ self.HostName @>, "")
-    let code = self.Factory.Backing(<@ self.Code @>, "")
+    let hostName = self.Factory.Backing(<@ self.HostName @>, "", notNullOrWhitespace)
+    let code = self.Factory.Backing(<@ self.Code @>, "", notNullOrWhitespace)
 
     member this.HostName
         with get() = hostName.Value
@@ -21,8 +22,9 @@ type OAuthViewModel(app: AppModel) as self =
         with get() = code.Value
         and set(v) = code.Value <- v
 
+    member val CurrentUser: (MoonFlower.Model.YourAccount option) = None with get, set
+
     member this.OpenAuthPage() = 
-        Debug.WriteLine (sprintf "open auth page %s" this.HostName)
         async {
             try
                 let! client = app.RegisterApp(this.HostName)
@@ -30,3 +32,14 @@ type OAuthViewModel(app: AppModel) as self =
                 Process.Start(url) |> ignore
             with | e -> Debug.WriteLine e
         } |> Async.Start
+
+    member this.AddAccount() =
+        async {
+            let! result = app.ConnectAccount(this.HostName, this.Code)
+            match result with
+            | None -> Debug.WriteLine "authentication failed"
+            | Some (auth, account) ->
+                this.CurrentUser <- Some account
+        }
+
+    member this.GetAccounts() = app.Accounts
